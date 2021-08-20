1d20e9e874acba80477e50f7e633853b11842824
RivaD2 <rivkadavidowski@gmail.com>
2021-08-20 12:25:17 -0700

    media queries addition for modal and edit to structure

--- src/modals/Modal.js
+++ src/modals/Modal.js
@@ -8,31 +8,30 @@ const Modal = ({ openModal, toggleModal, children }) => {
       <div className="modal-container">
         <div className="modal-content">
           <div className="modal-body">
-          <iframe
-            src="https://giphy.com/embed/3oEjHOyNwE2iz1z6bC"
-            title="giphy-cat"
-            width="480"
-            height="270"
-            frameBorder="0"
-            className="giphy-embed"
-            allowFullScreen>
-          </iframe>
-          <p><a href="https://giphy.com/gifs/originals-animation-kitten-logo-3oEjHOyNwE2iz1z6bC">via GIPHY</a></p>
-          <div className="modal-title">
-            <div>How to play:</div>
-          </div>
-          <div className="instructions">
-            <p> 1. Enter a search term and find your favorite GIFs</p>
-            <p> 2. Find the matching pairs of GIFs</p>
-            <p> 3. Goal is to find all pairs in the lowest amount of clicks</p>
-            <p> 3. Your best score will be saved!</p>
-          </div>
-          <div className="button-container">
-            <button className="close-modal-button" onClick={toggleModal}>
-              Close and Play
-            </button>
-          </div>
-        </div>
+            <iframe
+              src="https://giphy.com/embed/3oEjHOyNwE2iz1z6bC"
+              title="giphy-cat"
+              width="300"
+              height="300"
+              frameBorder="0"
+              className="giphy-embed">
+            </iframe>
+            <p><a href="https://giphy.com/gifs/originals-animation-kitten-logo-3oEjHOyNwE2iz1z6bC">via GIPHY</a></p>
+            <div className="modal-title">
+              <div>How to play:</div>
+            </div>
+            <div className="instructions">
+              <p> 1. Enter a search term and find your favorite GIFs</p>
+              <p> 2. Find the matching pairs of GIFs</p>
+              <p> 3. Goal is to find all pairs in the lowest amount of clicks</p>
+              <p> 3. Your best score will be saved!</p>
+            </div>
+            <div className="button-container">
+              <button className="close-modal-button" onClick={toggleModal}>
+                Close and Play
+              </button>
+            </div>
+         </div>
         </div>
       </div>
   )
